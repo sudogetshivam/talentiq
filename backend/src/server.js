b067040c6a5e0e21f0973858fbf607f32470c856
sudogetshivam <sorealgalaxy@gmail.com>
2025-12-17 01:22:12 +0530

    feat: integrate Inngest for user sync and deletion, add user model, and update environment variables
    - Added Inngest integration for handling user creation and deletion events from Clerk.
    - Created a new User model with fields for name, email, profile image, and Clerk ID.
    - Updated environment variables to include keys for Clerk and Inngest.
    - Configured CORS in the server to allow requests from the specified client URL.
    - Updated server to serve Inngest functions at the /api/inngest endpoint.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,27 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js";
+import cors from "cors"
+import {serve} from "inngest/express"
+import { functions, inngest } from "./lib/inngest.js";
+
 const app= express()
 
 const __dirname = path.resolve() /* this maintains the right address for any OS
 and dirname name is used to store the absolute path, deosnt matter from where you run the server, this si just a standarad variables */
 
+
+app.use(express.json()) //converts coming json data to Javascript Object so that we can read in req.bodt
+app.use(cors({
+    origin: ENV.CLIENT_URL,
+    credentials:true //allows cookies/tokens to enter or to come along with
+}))
 console.log(ENV.PORT)
 app.get('/',(req,res)=>{
    return res.status(200).json({msg:"sucess from api"})
 })
+
+app.use('/api/inngest',serve({client:inngest, functions}))
 
 app.get('/books',(req,res)=>{
    return res.status(200).json({msg:"This is the books endpoint"})
@@ -58,7 +70,7 @@ const startServer = async()=>{
     try{
     await connectDB() //if u remove await then also it will work but buttom code will execute first
     app.listen(3000,()=>
-        console.log("Server is running on port",ENV.PORT)
+        console.log(`Server is running at ${'https://talentiq-puhr.onrender.com'}`)
     )
     } catch(error){
         console.log("Error starting the server",error)
